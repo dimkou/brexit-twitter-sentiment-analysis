073355c7353288b76c3c98204934e52d6aaa0547
Dimitris Koutsoukos <dkoutsou@student.ethz.ch>
2019-05-19 10:20:33 +0200

    Add docstrings for classify.py

--- src/ml/classify.py
+++ src/ml/classify.py
@@ -17,11 +17,42 @@ from nltk.stem import WordNetLemmatizer
 
 
 class MLTweetSentiment:
+    """A class that takes a file path and trains a model performing sentiment
+    analysis on tweets (classifies a tweet as positive or negative).
+
+    Attributes:
+        inference_dataset_columns (list): The columns that have to be deleted
+            from the pandas frame of the inference sets.
+
+    """
     def __init__(self, train_data_path, inference_data_paths):
+        """The constructor of the class.
+
+        Args:
+            train_data_path (str): A string that contains the path for the
+                training set.
+            inference_data_paths (list): A list of strings that contains the
+                paths for the inference sets.
+        """
         self.train_data_path = train_data_path
         self.inference_data_paths = inference_data_paths
 
     def import_tweets(self, path, columns, delete_columns, delimiter=','):
+        """A function that takes a file path and converts it into a pandas frame
+        containing only the necessary information for training an ML algorithm.
+
+        Args:
+            path (str): The path of the file that has to be converted into a
+                pandas frame.
+            columns (list): The columns that will be prepended to the data
+                frame.
+            delete_columns (list): The columns that will be deleted from the
+                data frame.
+            delimiter (str): The delimiter of the data present in the file.
+
+        Returns:
+            dataset (pandas.frame): A pandas that is produced from a file path
+        """
         dataset = pd.read_csv(path, encoding='utf-8', delimiter=delimiter,
                               error_bad_lines=False, lineterminator='\n')
         dataset.columns = columns
@@ -40,6 +71,17 @@ class MLTweetSentiment:
         return dataset
 
     def preprocess_tweets(self):
+        """A function that by using the paths provided by the class imports all
+        the datasets as pandas frames. It also removes the unnecessary columns
+        from the training and inference dataset and returns these pandas
+        frames.
+
+        Returns:
+            train_dataset (pandas.frame): The dataset that will be used to
+                train the classifier.
+            inference_dataset (list): A list of pandas frames that will be used
+                for inference.
+        """
         train_dataset_columns = ['sentiment', 'id', 'date',
                                  'flag', 'user', 'text']
         train_dataset_delete_columns = ['flag', 'id', 'user', 'date']
@@ -50,7 +92,6 @@ class MLTweetSentiment:
         inference_dataset_delete_columns = ['date', 'score']
         inference_dataset = []
         for inference_data_path in self.inference_data_paths:
-            print(inference_data_path)
             inference_dataset.append(
                     self.import_tweets(
                         inference_data_path,
@@ -61,6 +102,16 @@ class MLTweetSentiment:
 
     @staticmethod
     def preprocess_tweet(tweet):
+        """A function that takes a tweet and applies a series of tranformation
+        so as to have the text as clean as possible for better vectorization
+        afterwards.
+
+        Args:
+            tweet (string): The text body of a tweet
+
+        Returns:
+            preprocessed_tweet (string): The text body of a preprocessed tweet
+        """
         tweet.lower()
         # convert all urls to string "URL"
         tweet = re.sub(r'((www\.[^\s]+)|(https?://[^\s]+))', 'URL', tweet)
@@ -80,15 +131,41 @@ class MLTweetSentiment:
         for word in tweet:
             temp_tweet.append(lemmatizer.lemmatize(stemmer.stem(word)))
 
-        return ' '.join(temp_tweet)
+        preprocessed_tweet = ' '.join(temp_tweet)
+        return preprocessed_tweet
 
     def convert_to_format_extract_features(self, train_dataset,
                                            inference_datasets,
                                            method, args):
+        """A function that takes a train set as a pandas frame and as a first
+        step extracts the text and the sentiment. Afterwards fits a vectorizer
+        to the train set and transforms and returns the whole training and
+        inference set using that vectorizer.
+
+        Args:
+            train_dataset (pandas.frame): The tweets in the training dataset.
+            inference_datasets (list): A list of pandas frames that contain the
+                inference datasets from the various brexit dates.
+            method (class): The method that is going to be used for
+                vectorization.
+            args (dict): The arguments that are going to be used by the
+                vectorizer.
+
+        Returns:
+            train_features (np.array): The array of vectors that constitutes
+                the training and test set.
+            labels (np.array): The labels of the train and test set.
+            inference_features (list): A list of np.arrays that has the
+                inference set.
+        """
         train_data = np.array(train_dataset.text)
         labels = np.array(train_dataset.sentiment)
+        # take advantage of the fact that the same random state produces the
+        # same split and shuffle procedure in scikit-learn
+        X_train, X_test, y_train, y_test = train_test_split(
+                train_data, labels, test_size=0.2, random_state=42)
         vector_transformer = method(**args)
-        vector_transformer.fit(train_data)
+        vector_transformer.fit(X_train)
         train_features = vector_transformer.transform(train_data)
         inference_features = []
         for inference_dataset in inference_datasets:
@@ -99,6 +176,30 @@ class MLTweetSentiment:
     def train_and_evaluate_classifier(self, train_features, labels,
                                       classifier, classifier_args,
                                       scaler):
+        """A function that takes the features, splits them into a train and
+        test set and does the same split for the labels. Afterwards, it trains
+        a model on the train set and a scaler if it's provided and returns some
+        metrics about the model performance, as well as the trained model and
+        scaler.
+
+        Args:
+            train_features (np.array): An array that contains the features that
+                the classifier needs to be trained on.
+            labels (np.array): An array that contains the labels that the
+                classifier needs to be trained on.
+            classifier (class): A class indicating the classifier that needs to
+                be trained.
+            classifier_args (dict): Arguments that need to be used by the
+                 classifier class.
+            scaler (class): A scaler that performs feature scaling.
+
+        Returns:
+            accuracy (float): The accuracy of the trained classifier on the
+                test set.
+            f1 (float): The F1 score of the trained classifier on the test set.
+            model (class): The trained model.
+            scaler (class): The trained scaler.
+        """
         X_train, X_test, y_train, y_test = train_test_split(
                 train_features, labels, test_size=0.2, random_state=42)
         model = classifier(**classifier_args)
@@ -113,11 +214,16 @@ class MLTweetSentiment:
         return accuracy, f1, model, scaler
 
     def evaluate_on_brexit(self, model, inference_features, scaler):
-        """A func
+        """A function that takes a trained model, a trained scaler and a list of
+        features and applies the trained model (and scaler) to the features in
+        order to perform inference.
 
-        :arg1:
-        :returns: None
-
+        Args:
+            model (class): A trained ML model.
+            inference_features (list):  A list that contains arrays that
+                represent the features calculated from the various Brexit
+                dates.
+            scaler (class): A trained scaler for the features.
         """
         for i, inference_feature in enumerate(inference_features):
             dataset = pd.read_csv(inference_dataset_paths[i],
@@ -138,20 +244,24 @@ class MLTweetSentiment:
 
 
 if __name__ == '__main__':
+    # Take all the necessary paths
     inference_dataset_paths = [
-        "../../data/test14-11-2018.csv",
-        "../../data/test15-01-2019.csv",
-        "../../data/test22-02-2016.csv",
-        "../../data/test23-06-2016.csv",
-        "../../data/test25-11-2018.csv",
-        "../../data/test19-06-2017.csv"]
+        "../../data/test14-11-2018.csv"]
+    # "../../data/test15-01-2019.csv",
+    # "../../data/test22-02-2016.csv",
+    # "../../data/test23-06-2016.csv",
+    # "../../data/test25-11-2018.csv",
+    # "../../data/test19-06-2017.csv"]
 
+    # Initialize an instance of the class with the paths
     twitterSentiment = MLTweetSentiment(
             "../../data/twitter_data.csv",
             inference_dataset_paths
             )
 
+    # Preprocess the tweets
     train_dataset, inference_datasets = twitterSentiment.preprocess_tweets()
+    # Set up your grid
     tfidf_args = {'stop_words': 'english', 'sublinear_tf': True}
     count_args = {'stop_words': 'english'}
     features_extractors = [TfidfVectorizer, CountVectorizer]
@@ -162,11 +272,13 @@ if __name__ == '__main__':
     naive_bayes_args = {}
     random_forest_args = {'n_estimators': [100, 300, 500],
                           'max_depth': [3, 5], 'n_jobs': [20]}
-    xgb_args = {'max_depth': [9, 11, 13], 'n_estimators': [2000, 1500], 
+    xgb_args = {'max_depth': [9, 11, 13], 'n_estimators': [2000, 1500],
                 'n_jobs': [16], 'learning_rate': [0.01, 0.1, 0.2, 1.0]}
     svm_args = {'max_iter': 100000, 'cache_size': 150000, 'verbosity': True}
     classifier_args = [logistic_args, naive_bayes_args, random_forest_args,
                        xgb_args, svm_args]
+    # Run the grid and print the score as well as the brexit tweets with
+    # annotation
     for i, feature_extractor in enumerate(features_extractors):
         for j, classifier in enumerate(classifiers):
             try:
